5991674e56741ead92dea30fe2c29164a8d200da
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-08-06 04:54:00 +0300

    Part 02 - Question (2)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -7,11 +7,25 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            // Question (1)
             ICircle Circle1 = new Circle(5.5);
             IRectangle Rectangle1 = new Rectangle(4.5, 7);
 
             Circle1.DisplayShapeInfo();
             Rectangle1.DisplayShapeInfo();
+
+            // Question (2)
+            IAuthenticationService authService = new BasicAuthenticationService();
+
+            string username = "user1";
+            string password = "123456";
+            string role = "Admin";
+
+            bool isAuthenticated = authService.AuthenticateUser(username, password);
+            Console.WriteLine($"Authentication result: {isAuthenticated}");
+
+            bool isAuthorized = authService.AuthorizeUser(username, role);
+            Console.WriteLine($"Authorization result: {isAuthorized}");
         }
     }
 }
