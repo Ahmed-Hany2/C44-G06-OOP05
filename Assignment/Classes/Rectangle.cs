a96a439800bcc450358e2a5d22bee568b5cac622
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-08-06 04:28:17 +0300

    Part 02 - Question (1)

---
+++ Assignment/Classes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using Assignment.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Classes
+{
+    public class Rectangle : IRectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Area => Width * Height;
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape: Rectangle");
+            Console.WriteLine($"Width: {Width}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Area: {Area:F2}");
+            Console.WriteLine();
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Assignment
+﻿using Assignment.Classes;
+using Assignment.Interfaces;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ICircle Circle1 = new Circle(5.5);
+            IRectangle Rectangle1 = new Rectangle(4.5, 7);
 
+            Circle1.DisplayShapeInfo();
+            Rectangle1.DisplayShapeInfo();
         }
     }
 }
